0fa313cf09ffb7ffb9ae43a3d86e58ebc4d75a06
gersualves775 <56324804+gersualves775@users.noreply.github.com>
2020-05-20 20:49:11 -0300

    Create framework-click.js

---
+++ framework-click.js
@@ -0,0 +1,6 @@
+$('#finalizar').on('click', $.colorbox({
+        open: false,
+        width:'100%',
+        Height:'100%',
+        href:"/wp-content/plugins/everso-ecommerce/views/register.php",
+  }));
