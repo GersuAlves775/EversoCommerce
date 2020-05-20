599bb4b6025ea322bdb912b913705dacb352e2ff
gersualves775 <56324804+gersualves775@users.noreply.github.com>
2020-05-20 20:51:10 -0300

    Create click.js

---
+++ click.js
@@ -0,0 +1,9 @@
+
+$('#finalizar').on('click', function(){
+  $.colorbox({
+        open: false,
+        width:'100%',
+        Height:'100%',
+        href:"/wp-content/plugins/everso-ecommerce/views/register.php",
+  })
+});
