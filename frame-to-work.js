3db907f943b29af2f84a1dce1d07816bc5aa5ba4
gersualves775 <56324804+gersualves775@users.noreply.github.com>
2020-05-20 20:18:55 -0300

    Create frame-to-work.js

---
+++ frame-to-work.js
@@ -0,0 +1,5 @@
+$('#modal-finish').iziModal({
+    iframe: true,
+    iframeHeight: 800,
+    iframeURL: "<a class="vglnk" href="http://izimodal.marcelodolza.com" rel="nofollow"><span>http</span><span>://</span><span>izimodal</span><span>.</span><span>marcelodolza</span><span>.</span><span>com</span></a>"
+});
